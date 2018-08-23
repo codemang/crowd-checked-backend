46c5e29dd11a78e9d410ab0c5da30879275eb605
Nathaniel Rubin <nateboop@gmail.com>
2018-08-22 21:54:40 -0400

    working index and post of highlights/comments

--- app/controllers/api/highlights_controller.rb
+++ app/controllers/api/highlights_controller.rb
@@ -1,8 +1,22 @@
 class Api::HighlightsController < ApplicationController
   skip_before_action :verify_authenticity_token
+
+  def index
+    response = Highlight.includes(:comments).where("url LIKE '%#{params[:url]}%'").each_with_object({}) do |highlight, memo|
+      memo[:highlights] ||= []
+      memo[:highlights] << {
+        body: highlight.body,
+        id: highlight.id,
+        comments: highlight.comments
+      }
+    end
+
+    render json: {response: response}
+  end
+
   def create
     highlight = Highlight.create!(body: params[:highlightedText], url: params[:url])
     Comment.create!(highlight: highlight, comment: params[:comment])
-    render json: {test: true}
+    render json: {highlight: highlight}
   end
 end
